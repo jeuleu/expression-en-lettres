4470a793164e889304b2560ad7f9bffdaf798ea4
Jean Lestang <jelestang@airfrance.fr>
2014-09-26 12:06:30 +0200

    Préparation vers github

--- spec/ConvertisseurSpec.js
+++ spec/ConvertisseurSpec.js
@@ -1,13 +1,37 @@
 describe( "Librairie de conversion d'expression", function () {
     describe( "convertit en lettres", function () {
-        it("nombre simple (2)", function () {
-         expect(ecritEnLettres(2)).toMatch(/deux/);
-        });	
-        it("nombre simple (3)	", function () {
-         expect(ecritEnLettres(3)).toMatch(/trois/);
-        });	
-        it("nombre simple", function () {
-         expect(ecritEnLettres(4)).toMatch(/quatre/);
-        });	
+	    describe( "les chiffres simples", function () {
+    	    it("2", function () {
+			 expect(ecritEnLettres(2)).toMatch("deux");
+			});	
+			
+			it("6", function () {
+			 expect(ecritEnLettres(6)).toMatch("six");
+			});	
+		});
+		
+	    describe( "les dizaines. Par exemple : 10", function () {
+			it("10", function () {
+			 expect(ecritEnLettres(10)).toMatch("dix");
+			});
+			
+// 			it("15", function () {
+// 			 expect(ecritEnLettres(15)).toMatch("quinze");
+// 			});	
+
+			it("17", function () {
+			 expect(ecritEnLettres(17)).toMatch("dix-sept");
+			});	
+		});	
+		
+	    describe( "les dizaines. Par exemple : 20", function () {
+			it("20", function () {
+			 expect(ecritEnLettres(20)).toMatch("vingt");
+			});	
+			
+			it("24", function () {
+			 expect(ecritEnLettres(24)).toMatch("vingt-quatre");
+			});	
+		});	
     });
 });
